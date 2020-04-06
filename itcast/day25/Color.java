902ca67d75031cc709b89fdf55f31221ab143c52
ziming <zimingnee@163.com>
2020-04-06 21:26:16 +0800

    demo更新

--- itcast/day25/Color.java
+++ itcast/day25/Color.java
@@ -1,5 +1,13 @@
 package itcast.day25;
 
+import java.util.Arrays;
+
 public enum Color {
     RED, BLUE, GREEN, BLACK;
+}
+class Main {
+    public static void main(String[] args) {
+        Color[] values = Color.values();
+        System.out.println(Arrays.toString(values));
+    }
 }
