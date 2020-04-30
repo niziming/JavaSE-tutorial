3036755cd5ce205bd88d03d0584804082a5f37a0
ziming <zimingnee@163.com>
2020-05-01 00:07:58 +0800

    EnumMap

--- itcast/chap22/Server.java
+++ itcast/chap22/Server.java
@@ -1,22 +1,48 @@
-package itcast.day22;
+package itcast.chap22;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8000);
-        Socket accept = serverSocket.accept();
-        InputStream inputStream = accept.getInputStream();
-        byte[] bytes = new byte[1080];
-        int len;
-        while ((len = inputStream.read(bytes)) != -1)
-            System.out.print(new String(bytes));
-        accept.close();
-        serverSocket.close();
-        inputStream.close();
+    // 定义线程池
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        service.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ServerSocket serverSocket = new ServerSocket(8000);
+                    Socket accept = serverSocket.accept();
+                    InputStream inputStream = accept.getInputStream();
+                    byte[] bytes = new byte[16];
+                    int len;
+                    int count = 0;
+                    while ((len = inputStream.read(bytes)) != -1){
+                        for (byte aByte : bytes) {
+                            System.out.printf("%02x ", aByte);
+                        }
+                        System.out.print(' ');
+                        for (byte aByte : bytes) {
+                            count++;
+                            System.out.printf("%c", aByte > 33 ? (char) aByte : '·');
+                        }
+                        if (count%16 == 0)System.out.println();
+                    }
+                    accept.close();
+                    serverSocket.close();
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
 }
