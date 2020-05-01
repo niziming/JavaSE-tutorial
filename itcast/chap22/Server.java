530163ff7c4c283fe34e308c23d73eb0eeddec0c
ziming <zimingnee@163.com>
2020-05-01 15:33:42 +0800

    server 打印字节

--- itcast/chap22/Server.java
+++ itcast/chap22/Server.java
@@ -13,36 +13,29 @@ public class Server {
     // 定义线程池
 
     public static void main(String[] args) {
-        ExecutorService service = Executors.newFixedThreadPool(4);
-        service.submit(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    ServerSocket serverSocket = new ServerSocket(8000);
-                    Socket accept = serverSocket.accept();
-                    InputStream inputStream = accept.getInputStream();
-                    byte[] bytes = new byte[16];
-                    int len;
-                    int count = 0;
-                    while ((len = inputStream.read(bytes)) != -1){
-                        for (byte aByte : bytes) {
-                            System.out.printf("%02x ", aByte);
-                        }
-                        System.out.print(' ');
-                        for (byte aByte : bytes) {
-                            count++;
-                            System.out.printf("%c", aByte > 33 ? (char) aByte : '·');
-                        }
-                        if (count%16 == 0)System.out.println();
-                    }
-                    accept.close();
-                    serverSocket.close();
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        try {
+            ServerSocket serverSocket = new ServerSocket(8000);
+            Socket accept = serverSocket.accept();
+            InputStream inputStream = accept.getInputStream();
+            byte[] bytes = new byte[16];
+            int len;
+            int count = 0;
+            while ((len = inputStream.read(bytes)) != -1) {
+                for (byte aByte : bytes) {
+                    System.out.printf("%02x ", aByte);
                 }
+                System.out.print(' ');
+                for (byte aByte : bytes) {
+                    count++;
+                    System.out.printf("%c", aByte > 33 ? (char) aByte : '·');
+                }
+                if (count % 16 == 0) System.out.println();
             }
-        });
-
+            accept.close();
+            serverSocket.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
