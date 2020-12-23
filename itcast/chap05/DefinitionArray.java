1c2542dac143d5d332cb93f80e007e5545c60497
taowuzhong <taowz@qq.com>
2020-12-23 13:53:41 +0800

    // 找出最大
      // 数组的反转

--- itcast/chap05/DefinitionArray.java
+++ itcast/chap05/DefinitionArray.java
@@ -1,21 +1,50 @@
 package itcast.chap05;
 
+import java.util.ArrayList;
+
 public class DefinitionArray {
     public static void main(String[] args) {
         // 数组存储的数据类型[] 数组名字 = new 数组存储的数据类型[长度];
-        int[] arr = new int[10];
-        int[] arr1 = new int[]{1, 2, 3};
-        int[] arr2 = {1, 2, 3};
-        ergodic(arr);
-        ergodic(arr1);
-        ergodic(arr2);
-        arr[0] = 6;
-        ergodic(arr);
+        // int[] arr = new int[10];
+        // int[] arr1 = new int[]{1, 2, 3};
+        // int[] arr2 = {1, 2, 3};
+        // ergodic(arr);
+        // ergodic(arr1);
+        // ergodic(arr2);
+        // arr[0] = 6;
+        // ergodic(arr);
+        // // arr = 10;
+        // arr = null;
+        // System.out.println(arr);
+        // int[] arr3 = { 5, 15, 2000, 10000, 100, 4000};
+        // int max = ArrayOperation.findMax(arr3);
+        // System.out.println(max);
+
+        // 找出最大
+        // int[] ints = randomArray(10, 100);
+        // ergodic(ints);
+        // int max = ArrayOperation.findMax(ints);
+        // System.out.println(max);
+
+        // 数组的反转
+        int[] ints = randomArray(10, 100);
+        ergodic(ints);
+        int[] rev = ArrayOperation.reverse(ints);
+        ergodic(rev);
+    }
+
+    private static int[] randomArray(int len, int actor) {
+        int[] arr4 = new int[len];
+        for (int i = 0; i < arr4.length; i++) {
+            arr4[i] = (int)(Math.random()*actor);
+        }
+        return arr4;
     }
 
     private static void ergodic(int[] arr) {
         for (int i : arr) {
             System.out.print(i);
+            System.out.print(' ');
         }
         System.out.println();
     }
