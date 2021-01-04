d6d65eeeafe80647a5058ec725de434f63444de2
taowuzhong <taowz@qq.com>
2021-01-04 16:20:38 +0800

    day19【File类、递归】-笔记

---
+++ itcast/chap19/demo1/FileUsed.java
@@ -0,0 +1,20 @@
+package itcast.chap19.demo1;
+
+import java.io.File;
+
+public class FileUsed {
+    public static void main(String[] args) {
+        // 构造方法
+        File file = new File("hello.txt");
+        System.out.println(file.getAbsolutePath());
+
+        String parent = "parent";
+        File file1 = new File(parent, "hello.txt");
+        System.out.println(file.getAbsolutePath());
+
+        File parent1 = new File("parent");
+        File file2 = new File(parent1, "hello.txt");
+        System.out.println(file.getPath());
+
+    }
+}
