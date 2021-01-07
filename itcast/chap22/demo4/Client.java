6c2639c2a0f909a8b4a28f2568fd5e8ec72b6fd1
taowuzhong <taowz@qq.com>
2021-01-07 10:09:15 +0800

    多客户端

--- itcast/chap22/demo4/Client.java
+++ itcast/chap22/demo4/Client.java
@@ -23,60 +23,33 @@ public class Client {
     void sendMsg(String msg) throws IOException {
         System.out.println(clientId + "发送" + msg);
         cbos.write(new String(clientId + ": " + msg).getBytes());
+
     }
 
     void close () throws IOException{
+        System.out.println("客户端关闭");
         cbos.close();
         s.close();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("客户端启动");
-        ExecutorService service = Executors.newFixedThreadPool(1);
-        // try (
-        //     Socket s = new Socket("", 6666);
-        //     BufferedOutputStream cbos = new BufferedOutputStream(s.getOutputStream());
-        // ) {
-        //     cbos.write("hello".getBytes());
-        // } catch (Exception e) {
-        //     e.printStackTrace();
-        // }
-
+        ExecutorService service = Executors.newFixedThreadPool(4);
         while (true) {
             service.submit(() -> {
                 Client client = new Client("", 6666);
                 try {
                     client.sendMsg("hello");
+                    client.close();
                     try {
-                        Thread.sleep(500);
+                        Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    client.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
         }
-        // Client client3 = new Client("", 6666);
-        // Client client4 = new Client("", 6666);
-        // client1.sendMsg("1");
-        // client2.sendMsg("2");
-        // client3.sendMsg("3");
-        // client4.sendMsg("4");
-        // while (true) {
-        //     Double random = Math.random() * 10.0;
-        //     Client client = new Client("", 6666);
-        //
-        //     service.submit(() -> {
-        //         client.sendMsg(random.toString());
-        //     });
-        //     try {
-        //         Thread.sleep(6000);
-        //     } catch (InterruptedException e) {
-        //         e.printStackTrace();
-        //     }
-        //     client.sendMsg("关闭连接");
-        // }
     }
 }
---
+++ itcast/chap23/FunctionInterface.java
@@ -0,0 +1,8 @@
+package itcast.chap23;
+
+// 函数式接口 函数式接口在Java中是指：有且仅有一个抽象方法的接口。
+public class FunctionInterface {
+    public static void main(String[] args) {
+
+    }
+}
