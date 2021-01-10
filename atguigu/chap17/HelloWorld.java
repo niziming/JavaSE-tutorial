01af62567f6efa314c7e8c0136601ecb0cbcfff9
ziming <zimingnee@163.com>
2021-01-10 19:39:34 +0800

    尚硅谷javase

---
+++ atguigu/chap17/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * @author songhongkang
+ * @create 2019 ���� 11:51
+ *
+ */
+public class HelloWorld {
+
+	public static void printHello() {
+		System.out.println("hello java");
+	}
+
+	public static void main(String[] args) {
+		printHello();
+	}
+}
